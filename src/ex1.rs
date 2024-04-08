d2387fa06930a3f3a8b86cb3ab8dccbbcd08b7c1
Igor Vaiman <igor.vaiman@inten.to>
2024-04-08 19:01:36 +0200

    tested bad mlcg

--- src/ex1.rs
+++ src/ex1.rs
@@ -11,6 +11,19 @@ struct MLCG {
     current: u32,
     step: usize,
 }
+impl Iterator for MLCG {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        self.step += 1;
+        (self.current, _) = self.current.overflowing_mul(self.mult);
+        if self.current != 0 && self.current != self.init {
+            return Some(self.current);
+        } else {
+            return None;
+        }
+    }
+}
 
 impl MLCG {
     fn new(init: u32, mult: u32) -> MLCG {
@@ -26,77 +39,66 @@ impl MLCG {
         self.current = self.init;
         self.step = 0;
     }
-}
 
-impl Iterator for MLCG {
-    type Item = u32;
+    fn run_tests(mut self, name: &str) -> () {
+        println!("\n\nTesgin {}", name);
+        self.reset();
 
-    fn next(&mut self) -> Option<u32> {
-        self.step += 1;
-        (self.current, _) = self.current.overflowing_mul(self.mult);
-        if self.current != self.init {
-            return Some(self.current);
-        } else {
-            return None;
+        let mut timer = std::time::Instant::now();
+
+        for _ in &mut self {} // exhausting the iterator
+
+        println!(
+            "Full iteration: {} steps (log2(steps) = {}), took {:.2} seconds",
+            self.step,
+            (self.step as f32).log2(),
+            timer.elapsed().as_secs_f32()
+        );
+
+        for sample_size in [
+            Some(2usize.pow(10)),
+            Some(2usize.pow(15)),
+            Some(2usize.pow(20)),
+            Some(2usize.pow(25)),
+            None,
+        ] {
+            println!("\nHistograming with sample size = {:?}", sample_size);
+            timer = std::time::Instant::now();
+            let mut hist = ndhistogram!(UniformNoFlow::new(100, 0.0, u32::MAX as f64));
+            self.reset();
+            for (idx, i) in (&mut self).enumerate() {
+                hist.fill(&(i as f64));
+                if let Some(limit) = sample_size {
+                    if idx == limit {
+                        break;
+                    }
+                }
+            }
+            println!(
+                "Histogram filled in {:.2} sec",
+                timer.elapsed().as_secs_f32()
+            );
+            let chi2_per_dof = uniform_chi2_per_dof(&hist, self.step);
+            println!("Histogram chi2 / d.o.f. = {}", chi2_per_dof);
+
+            let log2_size = (self.step as f64).log2();
+            plot_histogram(
+                &hist,
+                &format!(
+                    "{}, 2^{:.0} samples, chi2/dof = {:.2}",
+                    name, log2_size, chi2_per_dof
+                ),
+                "u32 value",
+                &format!("pics/ex1/{}-2^{:.0}.png", name, log2_size),
+            )
+            .expect("Failed to plot histogram");
         }
     }
 }
 
 pub fn uniform_random_sampling() {
-    let mut timer = std::time::Instant::now();
-
-    let mut mlcg = MLCG::new(987654321, 663608941);
-    // for _ in &mut mlcg {} // exhausting the iterator
-
-    println!(
-        "Full iteration: {} steps (log2(steps) = {}), took {:.2} seconds",
-        mlcg.step,
-        (mlcg.step as f32).log2(),
-        timer.elapsed().as_secs_f32()
-    );
-
-    for (sample_idx, sample_size) in [
-        Some(2usize.pow(8)),
-        Some(2usize.pow(10)),
-        Some(2usize.pow(15)),
-        Some(2usize.pow(20)),
-        Some(2usize.pow(25)),
-        None,
-    ]
-    .iter()
-    .enumerate()
-    {
-        println!("\nHistograming with sample size = {:?}", sample_size);
-        timer = std::time::Instant::now();
-        let mut hist = ndhistogram!(UniformNoFlow::new(100, 0.0, u32::MAX as f64));
-        mlcg.reset();
-        for (idx, i) in (&mut mlcg).enumerate() {
-            hist.fill(&(i as f64));
-            if let Some(limit) = sample_size {
-                if idx == *limit {
-                    break;
-                }
-            }
-        }
-        println!(
-            "Histogram filled in {:.2} sec",
-            timer.elapsed().as_secs_f32()
-        );
-        let chi2_per_dof = uniform_chi2_per_dof(&hist, mlcg.step);
-        println!("Histogram chi2 / d.o.f. = {}", chi2_per_dof);
-
-        plot_histogram(
-            &hist,
-            &format!(
-                "Simple MLCG, 2^{:.0} samples, chi2/dof = {:.2}",
-                (mlcg.step as f64).log2(),
-                chi2_per_dof
-            ),
-            "u32 value",
-            &format!("pics/1-simple-mlcg-{}.png", sample_idx),
-        )
-        .expect("Failed to plot histogram");
-    }
+    MLCG::new(987654321, 663608941).run_tests("Simple MLCG");
+    MLCG::new(10, 9).run_tests("Bad MLCG");
 }
 
 fn uniform_chi2_per_dof(
