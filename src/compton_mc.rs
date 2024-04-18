b7887ff43134af9bcc21934f4c832d5e4f21a04c
Igor Vaiman <igor.vaiman@inten.to>
2024-04-18 19:20:43 +0200

    backside images added

--- src/compton_mc.rs
+++ src/compton_mc.rs
@@ -1,4 +1,7 @@
+use bmp::{Image, Pixel};
 use rand::prelude::*;
+
+use ndhistogram::{axis::UniformNoFlow, ndhistogram, Histogram};
 
 use crate::{
     ex7::sample_exponential,
@@ -29,10 +32,12 @@ impl Vector3 {
         }
     }
 
-    fn add_inplace(&mut self, other: &Vector3) {
-        self.x += other.x;
-        self.y += other.y;
-        self.z += other.z;
+    fn add(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
     }
 }
 
@@ -42,7 +47,7 @@ struct Cuboid {
 }
 
 impl Cuboid {
-    fn is_inside(&self, point: &Vector3) -> bool {
+    fn contains(&self, point: &Vector3) -> bool {
         (self.min.x < point.x && point.x < self.max.x)
             && (self.min.y < point.y && point.y < self.max.y)
             && (self.min.z < point.z && point.z < self.max.z)
@@ -103,7 +108,7 @@ enum StepResult {
     ParticleRangedOut,
 }
 
-fn step(
+fn compton_mc_step(
     rng: &mut ThreadRng,
     photon: &mut Photon,
     material: &Material,
@@ -117,15 +122,16 @@ fn step(
     let lambda = mean_free_path(photon, material);
     let step_size = sample_exponential(rng, 1.0 / lambda);
     photon.direction.normalize();
-    photon.r.add_inplace(&photon.direction.mult(step_size));
-    if !material.geometry.is_inside(&photon.r) {
+    let next_interaction_point = photon.r.add(&photon.direction.mult(step_size));
+    if !material.geometry.contains(&next_interaction_point) {
         return StepResult::ParticleExited;
     }
+    photon.r = next_interaction_point;
     // simulating scattering
     let scattered_photon = sample_compton_scattering(rng, &photon.k());
     photon.energy *= scattered_photon.energy_ratio;
     let local_theta = scattered_photon.theta;
-    let local_phi = std::f32::consts::PI * rng.gen::<f32>();
+    let local_phi = 2.0 * std::f32::consts::PI * rng.gen::<f32>();
     // scattered particle direction in local frame, where inial particle went along Ox
     // standard vector pointing at (theta, phi)  but with z <-> x
     let (local_x, local_y, local_z) = (
@@ -150,24 +156,28 @@ fn step(
 }
 
 pub fn run_compton_mc() {
-    _run_compton_mc(100, 0.0);
-    _run_compton_mc(10000, 0.01);
-    _run_compton_mc(10000, 1.0);
+    // no absorption = photons do a long random walk before going out
+    _run_compton_mc(100, 500.0, 0.0);
+    // more or less realistic
+    _run_compton_mc(1_000_000, 200.0, 1.0);
+    _run_compton_mc(1_000_000, 800.0, 1.0);
+    _run_compton_mc(1_000_000, 3200.0, 1.0);
+    _run_compton_mc(1_000_000, 6400.0, 1.0);
+    _run_compton_mc(1_000_000, 12800.0, 1.0);
 }
 
-fn _run_compton_mc(total_photons: usize, min_energy: f32) {
-    let init_energy = 3.0 * 511.0; // KeV
+fn _run_compton_mc(total_photons: usize, initial_energy: f32, min_energy: f32) {
     let material = Material {
         geometry: Cuboid {
             min: Vector3 {
                 x: 0.0,
-                y: -5.0,
-                z: -5.0,
+                y: -2.0,
+                z: -2.0,
             },
             max: Vector3 {
                 x: 10.0,
-                y: 5.0,
-                z: 5.0,
+                y: 2.0,
+                z: 2.0,
             },
         },
         z: 82,
@@ -177,11 +187,25 @@ fn _run_compton_mc(total_photons: usize, min_energy: f32) {
 
     let mut rng = thread_rng();
 
+    let backside_image_size_px = 100;
+    let mut backside_image_hist = ndhistogram!(
+        UniformNoFlow::new(
+            backside_image_size_px,
+            material.geometry.min.y,
+            material.geometry.max.y
+        ),
+        UniformNoFlow::new(
+            backside_image_size_px,
+            material.geometry.min.z,
+            material.geometry.max.z
+        )
+    );
+
     let mut traces: Vec<Vec<(f32, f32, f32)>> = Vec::new();
     let mut exited_photons: usize = 0;
-    for idx in 0..total_photons {
+    for _idx in 0..total_photons {
         let mut photon = Photon {
-            energy: init_energy,
+            energy: initial_energy,
             r: Vector3 {
                 x: 0.0,
                 y: 0.0,
@@ -196,29 +220,35 @@ fn _run_compton_mc(total_photons: usize, min_energy: f32) {
         let mut trace: Vec<Vector3> = Vec::new();
         trace.push(photon.r.clone());
         loop {
-            let res = step(&mut rng, &mut photon, &material, &min_energy);
+            let res = compton_mc_step(&mut rng, &mut photon, &material, &min_energy);
             trace.push(photon.r.clone());
             if let StepResult::Continue = res {
                 continue;
             }
             if let StepResult::ParticleExited = res {
                 exited_photons += 1;
+                // adding exited photon to the histogram
+                if let Some(backside_intersection) = find_ray_x_plane_intersection(
+                    &photon.r,
+                    &photon.direction,
+                    &material.geometry.max.x,
+                ) {
+                    // println!("{} {}", backside_intersection.y, backside_intersection.z);
+                    backside_image_hist.fill(&(backside_intersection.y, backside_intersection.z));
+                }
             }
             break;
         }
-        println!("Particle #{} done in {} steps", idx, trace.len());
+
         traces.push(trace.into_iter().map(|r| (r.x, r.y, r.z)).collect());
     }
-    println!(
-        "Total particles exited from the material: {}",
-        exited_photons
-    );
+    println!("Total particles exited from material: {}", exited_photons);
 
     plot_lines_3d(
         traces,
         &format!(
-            "Compton-scattered photons (E={:.1}, N={}, Emin={:.1}, absorbed {:.2}%)",
-            init_energy,
+            "Compton-scattered photons (E={:.0} KeV, N={}, Emin={:.1}, absorbed {:.2}%)",
+            initial_energy,
             total_photons,
             min_energy,
             100.0 * (1.0 - (exited_photons as f32) / (total_photons as f32))
@@ -237,7 +267,68 @@ fn _run_compton_mc(total_photons: usize, min_energy: f32) {
                 material.geometry.max.z as f64,
             ),
         ),
-        &format!("out/compton/traces-emin={:.2}.png", min_energy),
+        &format!(
+            "out/compton/e={:.2}-emin={:.2}-3d-traces.png",
+            initial_energy, min_energy
+        ),
     )
     .expect("Failed to plot photon traces");
+
+    let max_bin_count = backside_image_hist
+        .iter()
+        .map(|item| item.value)
+        .fold(f64::MIN, |acc, v| v.max(acc)) as f32;
+    let mut img = Image::new(backside_image_size_px as u32, backside_image_size_px as u32);
+    let y_px_size =
+        (material.geometry.max.y - material.geometry.min.y) / (backside_image_size_px as f32);
+    let z_px_size =
+        (material.geometry.max.z - material.geometry.min.z) / (backside_image_size_px as f32);
+    for (y_px, z_px) in img.coordinates() {
+        let pixel_count = backside_image_hist
+            .value(&(
+                (material.geometry.min.y + (y_px as f32) * y_px_size),
+                (material.geometry.min.z + (z_px as f32) * z_px_size),
+            ))
+            .unwrap()
+            .clone() as f32;
+        let pixel_intensity = if pixel_count == 0.0 {
+            pixel_count
+        } else {
+            pixel_count.ln() / max_bin_count.ln()
+        };
+        let pixel_value = (255.0 * pixel_intensity) as u32;
+        img.set_pixel(y_px, z_px, px!(pixel_value, pixel_value, pixel_value));
+    }
+    let _ = img.save(&format!(
+        "out/compton/e={:.2}-emin={:.2}-backside-image.png",
+        initial_energy, min_energy
+    ));
+}
+
+fn find_ray_x_plane_intersection(
+    ray_start: &Vector3,
+    ray_dir: &Vector3,
+    plane_x: &f32,
+) -> Option<Vector3> {
+    let normal_dir_dot = ray_dir.x; // normal = (1, 0, 0)
+    if normal_dir_dot.abs() < 1e-15 {
+        // = ray is parallel to the plane (with some tolerance)
+        return None;
+    }
+
+    let t_intersection = (plane_x - ray_start.x) / normal_dir_dot;
+    if !t_intersection.is_finite() {
+        // something's not right!
+        return None;
+    }
+    if t_intersection < 0.0 {
+        // = ray goes away from the plane
+        return None;
+    }
+
+    Some(Vector3 {
+        x: ray_start.x + t_intersection * ray_dir.x,
+        y: ray_start.y + t_intersection * ray_dir.y,
+        z: ray_start.z + t_intersection * ray_dir.z,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate bmp;
 use clap::Parser;
 
 mod compton_mc;
@@ -21,6 +23,7 @@ struct CliArgs {
 
 fn main() {
     let args = CliArgs::parse();
+
     match args.exercise.as_str() {
         "1" => ex1::ex1(),
         "2" => ex2::ex2(),
