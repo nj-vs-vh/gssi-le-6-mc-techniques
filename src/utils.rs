5d92667390f08ee6b7d5997f914c3c5e25fcf1e3
Igor Vaiman <igor.vaiman@inten.to>
2024-05-16 12:01:06 +0200

    fix utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,15 @@
 pub fn mean_std(series: &[f32]) -> (f32, f32) {
-    let count = series.len() as f32;
+    let count = series.len() as f64;
     let (sum, square_sum) = series
         .iter()
-        .map(|v| (v, v.powi(2)))
+        .map(|v| {
+            let v64 = *v as f64;
+            (v64, v64.powi(2))
+        })
         .fold((0.0, 0.0), |(acc, acc_sq), (v, v_sq)| {
             (acc + v, acc_sq + v_sq)
         });
     let mean = sum / count;
     let var = square_sum / count - (sum / count).powi(2);
-    (mean, var.sqrt())
+    (mean as f32, var.sqrt() as f32)
 }
